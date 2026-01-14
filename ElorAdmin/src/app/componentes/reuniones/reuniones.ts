88601beb5dd43c9fc004431a90d6436ea24b5ee3
Yeray <yeray.fermosomo@elorrieta-errekamari.com>
2026-01-14 16:17:20 +0100

    subido esqueleto con el .gitignore definitivo

---
+++ ElorAdmin/src/app/componentes/reuniones/reuniones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reuniones',
+  imports: [],
+  templateUrl: './reuniones.html',
+  styleUrl: './reuniones.css',
+})
+export class Reuniones {
+
+}
